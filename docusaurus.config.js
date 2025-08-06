a38240e1dfd020066430aef629af0529569611b2
mmmajlis <admin@mmmajlis.com>
2025-08-06 16:08:42 +0530

    removing edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,8 +52,6 @@ const config = {
           sidebarItemsGenerator, // ← pass the function, not a string
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
@@ -63,8 +61,7 @@ const config = {
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
