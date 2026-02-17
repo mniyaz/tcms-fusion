6d0df759f0ade28f7fb0e3585af94365b54c8610
mniyaz <mohemmed.s@gmail.com>
2026-02-17 13:10:53 +0530

    updating logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -79,7 +79,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/tcms_logo.png',
       navbar: {
         logo: {
           alt: 'tcms logo',
